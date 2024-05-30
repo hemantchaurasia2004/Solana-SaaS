682cc3c4d8807679ff724a6cc2c51e873c525e82
Hemant Chaurasia <hemantchaurasia2004@gmail.com>
2024-05-30 18:17:08 +0530

    Database Setup/ AWS setup

---
+++ backend/src/index.ts
@@ -0,0 +1,5 @@
+import express from 'express';
+const app = express();
+
+
+// postgres + prisma => postgresql://neondb_owner:OfH9S0JgbZoE@ep-bold-sun-a5k7eu6p.us-east-2.aws.neon.tech/neondb?sslmode=require
